01874e8fcc65dcbe0f2cae34de298c1fdbf4493b
Kamil Saitov <saitov66@gmail.com>
2020-03-27 14:08:16 +0300

    + levelset segmentation, SITK output to numpy, 3d morph_snakes (in notebook, working way to slow)

--- src/segmentation/morph_snakes.py
+++ src/segmentation/morph_snakes.py
@@ -3,20 +3,25 @@ import morphsnakes as ms
 import numpy as np
 
 
-def morph_snakes_segmentation(snapshot, start_point: Tuple[int, int], start_radius: int, num_iterations: int, z=None):
+def morph_snakes_segmentation(snapshot,
+                              start_point: Tuple[int, int],
+                              start_radius: int,
+                              num_iterations: int,
+                              balloon=0,
+                              z=None):
     """
     Now only works with 2D image, thus always set z for now
     # TODO: do for 3D snapshot
     """
-    assert snapshot.ndim == 3
+    # assert snapshot.ndim == 3
 
-    img = snapshot[z]
+    img = snapshot
     # get the gaussian gradient for higher contrast
     gimg = ms.inverse_gaussian_gradient(img, alpha=5.0, sigma=5.0)
 
     # level_set is the starting
     level_set = ms.circle_level_set(img.shape, start_point, start_radius)
-    mask = ms.morphological_geodesic_active_contour(gimg.astype('float64'), num_iterations, level_set)
+    mask = ms.morphological_geodesic_active_contour(gimg.astype('float64'), num_iterations, level_set, balloon=balloon)
     # repeat the 2D mask so that it is the same size as the image
     mask = np.repeat(mask[np.newaxis, :, :], snapshot.shape[0], axis=0)
 
